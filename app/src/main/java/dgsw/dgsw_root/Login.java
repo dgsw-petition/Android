e1985b183419b72eac043c660ed4e2567ed2f733
KimGGun <rlarjs2020@dgsw.hs.kr>
2018-06-08 23:49:55 +0900

    ID 수정
    Fix

--- app/src/main/java/dgsw/dgsw_root/Login.java
+++ app/src/main/java/dgsw/dgsw_root/Login.java
@@ -1,7 +1,10 @@
 package dgsw.dgsw_root;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button to_sign = findViewById(R.id.login_to_sign_btn);
+        View.OnClickListener onClickListener = new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent nextIntent = new Intent(Login.this,Sign.class);
+                startActivity(nextIntent);
+            }
+        };
+
+        to_sign.setOnClickListener(onClickListener);
+
     }
 }
